1d577ff9a4aeed7e33a497e4d95a42669a9c27db
Pavlovic2003 <166434019+Pavlovic2003@users.noreply.github.com>
2024-06-01 16:06:40 +0200

    Update main.c

--- Software assembly/Core/Src/main.c
+++ Software assembly/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+// includes
 #include "LOLA.h"
 #include "AWG.h"
 #include "ProgRef.h"
